a9defb1f4b2d0348797c4b4b1a8874645df6a7bb
Brindusa Melinte <brindusa.melinte@gmail.com>
2019-07-02 20:36:07 -1100

    Reset button & check winner

--- p1.js
+++ p1.js
@@ -1,34 +1,45 @@
-/*function choose(){
-  var a=button.getAttribute('id');
-  if(a=='x'){
-    document.getElementById('test').innerHTML='You have choosen '+a+'. Make your first move!';
-    
-  }else if(a=='o'){
-    document.getElementById('test').innerHTML='You have choosen '+a+'. Make your first move!';
- 
-  }
-}*/
-
 var choosen='';
 
 function chooseX(){
   choosen='X';
-  document.getElementById('test').innerHTML='You have choosen '+choosen+'. Make your first move!';
+  document.getElementById('test').innerHTML='You have choosen '+ choosen +'. Make your move!';
 }
 
 function chooseO(){
   choosen='O';
-  document.getElementById('test').innerHTML='You have choosen '+choosen+'. Make your first move!';
+  document.getElementById('test').innerHTML='You have choosen '+ choosen +'. Make your move!';
 }
 
+var gameIsFinished=false;
 
 function returnSelected(t){
-  t.innerHTML=choosen;
+  if(gameIsFinished){
+    alert('Game is finished. Press "Click to play again!"');
+    return;
+  }  
+
+  if(t.innerHTML==''){
+    t.innerHTML=choosen;
+    var r=document.querySelectorAll('tr');
+    for(var i=0;i<r.length;i++){
+      var c=r[i].querySelectorAll('td');
+      if(c[0].innerHTML==c[1].innerHTML && c[0].innerHTML==c[2].innerHTML && c[0].innerHTML!=''){
+        alert('You have won!');
+	gameIsFinished=true;
+        return;
+      }
+    }
+ 
+  }else if(t.innerHTML!=''){
+          alert('Choose an empty box!');  
+        }  
 }
   
+
 function resetButton(){
   var r=document.querySelectorAll('td');
   for(var i=0;i<r.length;i++){
     r[i].innerHTML='';
+    gameIsFinished=false;
   }
 }
