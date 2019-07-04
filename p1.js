1c424ebf6f41bfee179e6014a19355be38f5d1b3
Brindusa Melinte <brindusa.melinte@gmail.com>
2019-07-04 10:21:53 -1100

    Function checkWinner

--- p1.js
+++ p1.js
@@ -3,49 +3,30 @@ var player;
 var score1=0;
 var score2=0;
 
-function startGame(){
-    document.getElementById('test').innerHTML='Player 1: X. Make your move!';   
+function startResetGame(){
+  document.getElementById('test').innerHTML='Player 1: X. Make your move!';   
+  var r=document.getElementById('game-table').querySelectorAll('td');
+  for(var i=0;i<r.length;i++){
+    r[i].innerHTML='';
+    gameIsFinished=false;
+  }
 }
 
 var gameIsFinished=false;
 
 function returnSelected(t){
   if(gameIsFinished){
-    alert('Game is finished. Press "Click to play again!"');
+    alert('Game is finished. Press Start / Reset button.');
     return;
   }  
 
   if(t.innerHTML==''){
     t.innerHTML=choosen;
-    var r=document.getElementById('game-table').querySelectorAll('tr');
-    for(var i=0;i<r.length;i++){
-      var c=r[i].querySelectorAll('td');
-      if(c[0].innerHTML==c[1].innerHTML && c[0].innerHTML==c[2].innerHTML && c[0].innerHTML!=''){
-        gameIsFinished=true;
-        alert('You have won '+player);
-        updateScore();
-        return;
-      }
+    if(checkWinner()){
+      alert('You have won '+player+'. Press Start / Reset button.');
+      updateScore();
+      gameIsFinished=true;
     }
-
-    var d=document.getElementById('game-table').querySelectorAll('td');
-    for(var j=0;j<3;j++){
-      if(d[j].innerHTML==d[j+3].innerHTML && d[j].innerHTML==d[j+6].innerHTML && d[j].innerHTML!=''){
-        gameIsFinished=true;
-        alert('You have won '+player);
-	updateScore();
-        return;
-      }    
-     }
-    
-    if((d[0].innerHTML==d[4].innerHTML && d[0].innerHTML==d[8].innerHTML && d[0].innerHTML!='')||
-       (d[2].innerHTML==d[4].innerHTML && d[2].innerHTML==d[6].innerHTML && d[2].innerHTML!='')){
-         gameIsFinished=true;
-         alert('You have won '+player);
-         updateScore();
-         return;
-      }
-    
   }else if(t.innerHTML!=''){
           alert('Choose an empty box!');
   }  
@@ -71,6 +52,28 @@ function switchPlayer(){
   }
 }
 
+function checkWinner(){
+    var r=document.getElementById('game-table').querySelectorAll('tr');
+    for(var i=0;i<r.length;i++){
+      var c=r[i].querySelectorAll('td');
+      if(c[0].innerHTML==c[1].innerHTML && c[0].innerHTML==c[2].innerHTML && c[0].innerHTML!=''){
+        return true;
+      }
+    }
+
+    var d=document.getElementById('game-table').querySelectorAll('td');
+    for(var j=0;j<3;j++){
+      if(d[j].innerHTML==d[j+3].innerHTML && d[j].innerHTML==d[j+6].innerHTML && d[j].innerHTML!=''){
+        return true;
+      }    
+     }
+    
+    if((d[0].innerHTML==d[4].innerHTML && d[0].innerHTML==d[8].innerHTML && d[0].innerHTML!='')||
+       (d[2].innerHTML==d[4].innerHTML && d[2].innerHTML==d[6].innerHTML && d[2].innerHTML!='')){
+         return true;
+      }
+}
+
 function updateScore(){
   document.getElementById('score-table').querySelectorAll('td')[0].innerHTML=score1;
   document.getElementById('score-table').querySelectorAll('td')[1].innerHTML=score2;
@@ -94,12 +97,4 @@ function allBoxFilled(){
     }
     return allBoxFilled;
 } 
-
-function resetButton(){
-  var r=document.getElementById('game-table').querySelectorAll('td');
-  for(var i=0;i<r.length;i++){
-    r[i].innerHTML='';
-    gameIsFinished=false;
-  }
-}
 
